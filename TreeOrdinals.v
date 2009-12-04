3b0e5201bb27dc98a4b953f1c4751ab7b18216f6
Martijn Vermaat <martijn@vermaat.name>
2009-12-04 16:07:06 +0000

    Renamed and cleaned some definitions to be more sane. Proved reflexivity of ord_le.

--- TreeOrdinals.v
+++ TreeOrdinals.v
@@ -7,61 +7,45 @@
   We can also choose to represent a limit by a stream instead of a function...
 *)
 
-Inductive Ord : Set :=
-  | Zero  : Ord
-  | Succ  : Ord -> Ord
-  | Limit : (nat -> Ord) -> Ord.
+Inductive ord : Set :=
+  | Zero  : ord
+  | Succ  : ord -> ord
+  | Limit : (nat -> ord) -> ord.
 
-(* Below are the definitions by muad (Coq-Club, 2009-11-25) for the Peter
-   Hancock notes. He mentions that already proving reflexivity of <= seems
-   hard.
+Definition fam (A : Type) : Type := { I : Set & I -> A }.
+Definition pow (A : Type) : Type := A -> Set.
 
-   NB: I don't claim to understand the Peter Hancock notes at this time. *)
-
-Definition Fam (A : Type) : Type := { I : Type & I -> A }.
-Definition Pow (A : Type) : Type := A -> Set.
-
-Fixpoint Pd (o : Ord) : Type :=
-  match o with
-  | Zero    => False
-  | Succ o' => (unit + Pd o') % type
-  | Limit l => { n : nat & Pd (l n) }
+Fixpoint pred_type (alpha : ord) : Set :=
+  match alpha with
+  | Zero       => False
+  | Succ alpha => (unit + pred_type alpha) % type
+  | Limit f    => { n : nat & pred_type (f n) }
   end.
 
-Definition zero z := Pd z -> False.
+Definition zero z := pred_type z -> False.
 
 Notation "!" := (False_rect _).
 
-Fixpoint Pd_pd (o : Ord) : Pd o -> Ord :=
-  match o with
-  | Zero    => !
-  | Succ o' => fun i => match i with
-                        | inl tt => o'
-                        | inr t  => Pd_pd o' t
-                        end
-  | Limit l => fun i => match i with
-                        | existT n t => Pd_pd (l n) t
+Fixpoint pred (alpha : ord) : pred_type alpha -> ord :=
+  match alpha with
+  | Zero       => !
+  | Succ alpha => fun i => match i with
+                           | inl tt => alpha
+                           | inr t  => pred alpha t
+                           end
+  | Limit f    => fun i => match i with
+                        | existT n t => pred (f n) t
                         end
   end.
 
-Definition pd (o : Ord) : Fam Ord := existT _ (Pd o) (Pd_pd o).
+Definition pd (alpha : ord) : fam ord := existT _ (pred_type alpha) (pred alpha).
 
-(*
-Fixpoint Ord_le (o p : Ord) : Type :=
-  match o with
-  | Zero    => unit
-  | Succ o' => { t : Pd p & Ord_le o' (Pd_pd p t) }
-  | Limit l => forall n : nat, Ord_le (l n) p
-  end.
-*)
-
-Inductive Ord_le : Ord -> Ord -> Type :=
-  | Ord_le_Zero : forall o, Ord_le Zero o
-  | Ord_le_Succ : forall (alpha beta : Ord) (p : Pd beta),
-                  Ord_le alpha (Pd_pd beta p) -> Ord_le (Succ alpha) beta
-  | Ord_le_Limit : forall (f : nat -> Ord) (beta : Ord), 
-                   (forall n, Ord_le (f n) beta) -> Ord_le (Limit f) beta. 
-
+Inductive ord_le : ord -> ord -> Prop :=
+  | Ord_le_Zero : forall alpha, ord_le Zero alpha
+  | Ord_le_Succ : forall (alpha beta : ord) (i : pred_type beta),
+                  ord_le alpha (pred beta i) -> ord_le (Succ alpha) beta
+  | Ord_le_Limit : forall (f : nat -> ord) (beta : ord),
+                   (forall n, ord_le (f n) beta) -> ord_le (Limit f) beta.
 
 (*
 Lemma kantine :
@@ -71,42 +55,40 @@ Proof.
 intros.
 inversion_clear X.
 (* ? *)
-
 *)
 
-Lemma le_LimO_r : forall alpha f n,
-  Ord_le alpha (f n) ->
-  Ord_le alpha (Limit f).
+(* Bruno Barras Lemma *)
+Lemma ord_le_Limit_r : forall alpha f n,
+  ord_le alpha (f n) ->
+  ord_le alpha (Limit f).
 Proof.
-induction alpha.
-intros.
+induction alpha as [| alpha IHs | f IHl]; intros g n H.
 constructor.
-intros f n H.
 inversion_clear H.
-apply Ord_le_Succ with (p:=existT (fun n => Pd (f n)) n p).
-simpl.
+apply Ord_le_Succ with (i := existT (fun n => pred_type (g n)) n i).
 assumption.
-intros.
 constructor.
 intro m.
-apply (X m f n).
-inversion_clear X0.
-apply X1.
+apply (IHl m g n).
+inversion_clear H.
+apply H0.
 Qed. 
 
-Lemma Ord_le_refl : forall o : Ord, Ord_le o o.
+Lemma ord_le_refl : forall alpha : ord, ord_le alpha alpha.
 Proof.
-induction o.
-simpl.
-exact tt.
-
+induction alpha as [| alpha IHs | f IHl].
 constructor.
-inversion IHo.
-simpl.
+apply Ord_le_Succ with (i := inl (pred_type alpha) tt).
+assumption.
+constructor.
+intro n.
+apply ord_le_Limit_r with (n := n).
+apply IHl.
+Qed.
 
 
-
+(*
 Definition Ord_eq (o p : Ord) := (Ord_le o p, Ord_le p o).
 
 Definition Ord_lt (o p : Ord) := { t : Pd p & Ord_le o (Pd_pd p t) }.
-
+*)
